b0737b2cca858b23caf3d0446b11df42e75318a6
blessingtb3 <blessingtb3@gmail.com>
2025-02-06 15:58:06 +0200

    feat: implementing arrow function to display Hello Blessing

--- rjs-movie-app/src/App.js
+++ rjs-movie-app/src/App.js
@@ -1,26 +1,14 @@
 //our app functional component written in jsx 
 
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+//using an arrow function component that returns App
+const App = () => {
   return (
-    <div className="App">{/* in html class element, in jsx we define and refer to classes by using className element */}
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code React
-        </a>
-      </header>
+    <div className="App">
+      <h1>Hello Blessing</h1>
     </div>
+
   );
 }
 
--- rjs-movie-app/src/index.js
+++ rjs-movie-app/src/index.js
@@ -1,20 +1,18 @@
 //This app indicates the starting point of our js application
 
 import React from 'react';
-import ReactDOM from 'react-dom/client';//calling react dom
+import ReactDOM from 'react-dom/client'; //calling react dom
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//calling the render method from the ReactDom package, passing our app and document.getElementById arguments in
+//calling the createRoot method from the ReactDOM package, passing our app and document.getElementById arguments in
 //react dom is used to render our components and our entire app into the real dom, into our div with the root id.
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
-
-
 
 reportWebVitals();
